e6ec0ef612c75da6fd9d86f8f13543d920bd4e5f
Caiwei Wang <43483829+wendyw1999@users.noreply.github.com>
2021-08-17 16:13:10 -0700

    Updates

---
+++ precache-manifest.9efba1ee2eaaf83b0f5ffc80719e03c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0464d3ff8a9761d5593377f3690ef301",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "70b061822ab4061f51c7",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "494705edd8ee88c72e4b",
+    "url": "/home/static/js/2.4079b26c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.4079b26c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b061822ab4061f51c7",
+    "url": "/home/static/js/main.4ff48ae1.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "87bb877c76cc436274b5bcd23d92c9ce",
+    "url": "/home/static/media/graduation_sunset_pic.87bb877c.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
