453395d035677bb72a1aaa83e0a0f89937048219
Caiwei Wang <43483829+wendyw1999@users.noreply.github.com>
2021-08-17 16:10:27 -0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const Footer = (props) => {
+  const bgStyle = { backgroundColor: "#f5f5f5" };
+
+  return (
+    <footer style={bgStyle} className="mt-auto py-5 text-center ">
+     
+    </footer>
+  );
+};
+
+export default Footer;
